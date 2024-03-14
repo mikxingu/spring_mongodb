8ed11b78c0610e5b34f2e3d315ef48b6bbfd41ef
Mike Alves <michelalvs@gmail.com>
2024-03-14 19:23:26 -0300

    Completed Lecture 339 - Database Instantiation

--- src/main/java/com/mikxingu/SpringMongodbApplication.java
+++ src/main/java/com/mikxingu/SpringMongodbApplication.java
@@ -8,8 +8,6 @@ public class SpringMongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongodbApplication.class, args);
-		
-		System.out.println("BLAH");
-	}
 
+	}
 }
