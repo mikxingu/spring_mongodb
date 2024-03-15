7e26ff063b39fbba133d0b6920f2cce435ccc591
Mike Alves <michelalvs@gmail.com>
2024-03-15 18:50:13 -0300

    Completed Lecture 351 - Adding comments to posts

--- src/main/java/com/mikxingu/domain/Post.java
+++ src/main/java/com/mikxingu/domain/Post.java
@@ -1,12 +1,15 @@
 package com.mikxingu.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 
 import com.mikxingu.dto.AuthorDTO;
+import com.mikxingu.dto.CommentDTO;
 
 public class Post implements Serializable{
 
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
