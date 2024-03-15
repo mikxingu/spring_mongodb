b09ed171caab2556dec8f7b66588cb1d506edcce
Mike Alves <michelalvs@gmail.com>
2024-03-15 11:26:41 -0300

    Completed Lecture 340 - UserDTO Creation

--- src/main/java/com/mikxingu/controllers/UserController.java
+++ src/main/java/com/mikxingu/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.mikxingu.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mikxingu.domain.User;
+import com.mikxingu.dto.UserDTO;
 import com.mikxingu.services.UserService;
 
 @RestController()
@@ -19,10 +21,13 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List<User> users = userService.findAll();
 		
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> userDtos = 
+				users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(userDtos);
 	}
 }
