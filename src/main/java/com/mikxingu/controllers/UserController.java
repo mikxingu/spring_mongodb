16e61f38ac9139f0df14191c6e98a167a27d0214
Mike Alves <michelalvs@gmail.com>
2024-03-15 11:38:04 -0300

    Completed Lecture 341 - Obtaining a user by Id

--- src/main/java/com/mikxingu/controllers/UserController.java
+++ src/main/java/com/mikxingu/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,18 +17,25 @@ import com.mikxingu.services.UserService;
 @RestController()
 @RequestMapping(value = "/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
-		
+	public ResponseEntity<List<UserDTO>> findAll() {
+
 		List<User> users = userService.findAll();
-		
-		List<UserDTO> userDtos = 
-				users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		
+
+		List<UserDTO> userDtos = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+
 		return ResponseEntity.ok().body(userDtos);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+		User obj = userService.findById(id);
+
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/mikxingu/services/UserService.java
+++ src/main/java/com/mikxingu/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mikxingu.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mikxingu.domain.User;
 import com.mikxingu.repositores.UserRepository;
+import com.mikxingu.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,15 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 		
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Usuário não encontrado");
+		}
+		return user.get();
 	}
 
 }
