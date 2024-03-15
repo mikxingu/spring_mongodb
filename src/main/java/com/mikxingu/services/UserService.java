0ec54def2eed09f2c398b6ceaa1bb319144975c6
Mike Alves <michelalvs@gmail.com>
2024-03-15 16:40:19 -0300

    Completed Lecture 344 - User Update with put

--- src/main/java/com/mikxingu/services/UserService.java
+++ src/main/java/com/mikxingu/services/UserService.java
@@ -13,17 +13,17 @@ import com.mikxingu.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
-		
+
 	}
-	
+
 	public User findById(String id) {
-		
+
 		Optional<User> user = userRepository.findById(id);
 		if (user == null) {
 			throw new ObjectNotFoundException("Usuário não encontrado");
@@ -34,12 +34,29 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = userRepository.findById(obj.getId()).get();
+		
+		if (newObj != null) {
+			updateUser(newObj, obj);
+			return userRepository.save(newObj);
+		} else {
+			throw new ObjectNotFoundException("Usuario não encontrado.");
+		}
+		
+	}
+
+	private void updateUser(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
